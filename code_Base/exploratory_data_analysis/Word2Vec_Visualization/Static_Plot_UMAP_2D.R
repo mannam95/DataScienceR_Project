af71a701de698a56e4438539b544d4e1c75ac38c
psingh1n <82735322+psingh1n@users.noreply.github.com>
2021-07-03 16:27:52 +0200

    Add files via upload

---
+++ code_Base/exploratory_data_analysis/Word2Vec_Visualization/Static_Plot_UMAP_2D.R
@@ -0,0 +1,31 @@
+
+#importing required libraries
+library(dplyr)
+library(word2vec)
+library(stylo)
+library(ggplot2)
+library(ggrepel)
+
+#setting the value of path variable to feed to the word2vec model
+#change path accordingly
+path  <- "/Users/priyankasingh/tweets.csv"
+
+#Setting Hyper-parameters for Model training
+set.seed(10034)
+
+
+#training the model with dimensions as 25 and 20 iterations
+model <- word2vec(path, dim = 25, iter = 20)
+
+#embedding for word2vec is created after model is trained
+embedding <- as.matrix(model)
+
+## Creating a Static plot for HateSpeech Vector 
+df  <- data.frame(word = gsub("//.+", "", rownames(dendo)), 
+                  xpos = gsub(".+//", "", rownames(dendo)), 
+                  x = viz[, 1], y = viz[, 2], 
+                  stringsAsFactors = FALSE)
+
+ggplot(df, aes(x = x, y = y, label = word)) + 
+  geom_text_repel() + theme_void() + 
+  labs(title = "word2vec - Visualization of Embeddings in 2D using UMAP")
